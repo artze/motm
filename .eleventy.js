ff525e4c9f378a6f2f67ae64e18c23dfcd62e28e
artze <13703523+artze@users.noreply.github.com>
2024-06-16 11:25:48 +0800

    Configure dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,15 @@
+/** @param {import('@11ty/eleventy').UserConfig} eleventyConfig */
 module.exports = function (elventyConfig) {
   elventyConfig.addPassthroughCopy({
     "./public/": "/",
   });
+
+  return {
+    dir: {
+      input: "content",
+      includes: "../_includes",
+      data: "../_data",
+      output: "_site",
+    },
+  };
 };
