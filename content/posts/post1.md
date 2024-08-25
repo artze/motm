5f04f2d04f81c88f72dfb467b5e4f2bbb9125a81
artze <13703523+artze@users.noreply.github.com>
2024-08-25 19:14:28 +0200

    Remove boilerplate content

--- content/posts/post1.md
+++
@@ -1,8 +0,0 @@
----
-layout: base.njk
-title: Post One
----
-
-# Post One
-
-A quick brown fox.
--- content/posts/posts.11tydata.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  permalink: function ({ title }) {
-    return `/posts/${this.slugify(title)}`;
-  },
-};
